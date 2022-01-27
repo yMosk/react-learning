a3b40f216698619ef1654b7e5f628f8773ba9d7b
Yana Moskalova <yanamoskalova@Yanas-MBP.lan>
2022-01-26 16:38:24 -0800

    initial commit

---
+++ learn-redux/README.md
@@ -0,0 +1,9 @@
+//REDUX --> state management library primarily used for a large scale applications as it has a lot of boilerplate code
+
+//STORE --> globalized state
+
+//ACTION --> function that returns an object with type or name, can also have payload
+
+//REDUCER --> checks what Action was executed and updates Store
+
+//DISPATCH --> where you execute Action
---
+++ learn-redux/src/App.js
@@ -0,0 +1,19 @@
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
+
+function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      {isLogged ? <h3>Only visible if you are logged in</h3> : ""}
+    </div>
+  );
+}
+
+export default App;
